5247a963bb270787314a1bb705d445fa62a82daa
Andrew Schutt <schutt.andrew@gmail.com>
2018-11-30 20:42:39 -0600

    part2 solution

--- intro/part2/src/Main.elm
+++ intro/part2/src/Main.elm
@@ -7,18 +7,13 @@ import Html.Attributes exposing (..)
 viewTags tags =
     let
         renderedTags =
-            -- 👉 TODO: use `List.map` and `viewTag` to render the tags
-            []
+            List.map (viewTag) tags
     in
     div [ class "tag-list" ] renderedTags
 
 
 viewTag tagName =
-    {- 👉 TODO: render something like this:
-
-       <button class="tag-pill tag-default">tag name goes here</button>
-    -}
-    button [] []
+    button [class "tag-pill tag-default"] [text tagName]
 
 
 main =
@@ -26,6 +21,8 @@ main =
         tags =
             [ "elm", "fun", "programming", "compilers" ]
     in
+
+    
     div [ class "home-page" ]
         [ viewBanner
         , div [ class "container page" ]
@@ -34,9 +31,7 @@ main =
                 , div [ class "col-md-3" ]
                     [ div [ class "sidebar" ]
                         [ p [] [ text "Popular Tags" ]
-
-                        -- 👉 TODO: instead of passing [] to viewTags, pass the actual tags
-                        , viewTags []
+                        , viewTags tags
                         ]
                     ]
                 ]
